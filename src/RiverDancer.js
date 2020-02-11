82c3b6587b4067a1fbbed8e702e278117bc91df9
phonxaychampasouk <phonxay.champasouk@outlook.com>
2020-02-10 18:08:32 -0800

    Implement RiverDancer.

---
+++ src/RiverDancer.js
@@ -0,0 +1,17 @@
+var RiverDancer = function (top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+
+};
+
+RiverDancer.prototype = Object.create(Dancer.prototype);
+
+RiverDancer.prototype.constructor = RiverDancer;
+
+RiverDancer.prototype.step = function () {
+  // call the old version of step at the beginning of any call to this new version of step
+  Dancer.prototype.step.call(this);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.fadeToggle();
+};
--- src/init.js
+++ src/init.js
@@ -27,6 +27,9 @@ $(document).ready(function() {
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+
+    dancer.$node.addClass(dancerMakerFunctionName);
+
     $('body').append(dancer.$node);
   });
 });
