5735ee60861efbbfa4c952916c0c6fd2865c5cae
phonxaychampasouk <phonxay.champasouk@outlook.com>
2020-02-10 18:13:51 -0800

    Implement LineDancer basic functionality.

---
+++ src/LineDancer.js
@@ -0,0 +1,17 @@
+var LineDancer = function (top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+
+};
+
+LineDancer.prototype = Object.create(Dancer.prototype);
+
+LineDancer.prototype.constructor = LineDancer;
+
+LineDancer.prototype.step = function () {
+  // call the old version of step at the beginning of any call to this new version of step
+  Dancer.prototype.step.call(this);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.fadeToggle();
+};
