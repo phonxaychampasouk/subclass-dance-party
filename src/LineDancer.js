f46cf40d50e13083c7d2a8007982be07bd0ed6be
phonxaychampasouk <phonxay.champasouk@outlook.com>
2020-02-11 11:56:04 -0800

    Implement growing Pac-Man up to size 8. Fix wall bouncing bug.

--- src/LineDancer.js
+++ src/LineDancer.js
@@ -5,7 +5,7 @@ var LineDancer = function () {
   this.direction = Math.random(0, 2) * Math.PI;
   this.xmove = Math.sin(this.direction);
   this.ymove = Math.cos(this.direction);
-
+  this.size = 2;
   Dancer.call(this, this.top, this.left, this.timeBetweenSteps);
 
 
@@ -22,36 +22,38 @@ LineDancer.prototype.step = function () {
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
   // this.$node.fadeToggle();
-
-  if (this.left > ($("body").width() - 40)) {
+  if (this.left > ($("body").width() - (20 * this.size))) {
     // if it hit the right wall
     // change direction
-    this.xmove = -this.xmove;
+    this.xmove = -Math.abs(this.xmove);
   } else if (this.top < 32) {
     // if it hit the top wall
     // change direction
-    this.ymove = -this.ymove;
+    this.ymove = Math.abs(this.ymove);
   } else if (this.left < 0) {
     // if it hit the left wall
     // change direction
-    this.xmove = -this.xmove;
-  } else if (this.top > $("body").height() - 40) {
+    this.xmove = Math.abs(this.xmove);
+  } else if (this.top > $("body").height() - (20 * this.size)) {
     // if it hit the bottom wall
     // change direction
-    this.ymove = -this.ymove;
+    this.ymove = -Math.abs(this.ymove);
   }
 
   // check all of the dots on the page
   for (var i=0; i < window.blinkyDancers.length; i++) {
     // this.top and this.left is our position
     // window.blinkyDancers[i].top and window.blinkyDancers[i].left is their position
-    if (window.blinkyDancers[i].left > this.left && window.blinkyDancers[i].left < this.left+30
-      && window.blinkyDancers[i].top > this.top && window.blinkyDancers[i].top < this.top+30) {
+    if (window.blinkyDancers[i].left > this.left && window.blinkyDancers[i].left < (this.left + (20 * this.size - 10))
+      && window.blinkyDancers[i].top > this.top && window.blinkyDancers[i].top < (this.top + (20 * this.size - 10))) {
       // remove the blinky dancer
-        window.blinkyDancers[i].$node.remove();
-        window.blinkyDancers.splice(i,1);
+      window.blinkyDancers[i].$node.remove();
+      window.blinkyDancers.splice(i,1);
       // and grow
-      
+      if (this.size < 8) {
+        this.size++;
+      }
+      this.$node.addClass("linesize"+this.size);
     }
   }
 
